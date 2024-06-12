b660029fda9887a4ef191b1dc6dd6814e5f8e36d
nafisa <nafisaabdurakhmon@gmail.com>
2024-06-12 14:46:43 +0500

    update

---
+++ funtion/.js
@@ -0,0 +1,17 @@
+
+
+
+function checkPassword(password){
+    let safepassword = 1991;
+    if(password === safepassword){
+        console.log("phone is unlocked")
+     }
+     else{
+        console.log('phone is loked')
+         }
+}
+
+
+
+checkPassword(1991)
+
